9ebc252580766650dab638ff25b6a59fd06bf70f
glebeckaya <glebeckaya@xbsoftware.by>
2021-04-07 19:04:28 +0300

    error correction
    files fixed according to comments

---
+++ src/index.js
@@ -0,0 +1,79 @@
+// header
+var header = {
+    type: "clean",
+    view: "toolbar", 
+    css:"webix_dark", 
+    padding: 10, 
+    elements: [
+        { view: "label", label:"My App"},
+        {view:"button", id:"profile-button", type: "icon", icon:"mdi mdi-account", label:"Profile", autowidth: true, css: "webix_transparent"}
+    ]
+};
+
+//main
+const side = {
+    type: "clean",
+    css: "side-section",
+    minWidth: 200,
+        rows:[
+            {
+                view:"list",
+                width:200,
+                autoheight:true,
+                scroll:false,
+                data:[ "Dashboard", "Users", "Products", "Locations" ]
+            },
+            { },
+            { template:"<span class='webix_icon mdi mdi-check'></span>Connected", autoheight:true}
+        ]
+};
+
+const base = {
+  minWidth: 750,
+  rows: [
+    {
+      view:"datatable",
+      data:small_film_set,
+      autoConfig:true
+    }
+  ]
+}
+
+const form = {
+  type: "clean",
+  rows: [
+    {
+      view:"form",
+      id: "form-films",
+      width:300,
+      elements:[
+        { template: "Edit films", type: "section"},
+        { view:"text", label:"Title"},
+        { view:"text", type:"number", label:"Year"},
+        { view:"text", type:"number", label:"Rating"},
+        { view:"text", type:"number", label:"Votes"},
+        { margin:5, cols:[
+            { view:"button", value:"Add new" , css:"webix_primary"},
+            { view:"button", value:"Clear"}
+        ]}
+      ]
+    },
+  {},
+  ]
+}
+
+const main = {
+    cols:[
+        side,
+        {view: "resizer"},
+        base,
+        form,
+    ]
+};
+
+//footer
+const footer = { template: "The software is provided by <a href='https://webix.com' target='blank'>https://webix.com.</a> All rights reserved (c)", autoheight: true, css: "footer"}
+
+webix.ui({
+    rows: [header, main, footer]
+});
--- src/testdata.js
+++ src/testdata.js
@@ -1,4 +1,4 @@
-var small_film_set = [
+const small_film_set = [
 	{ id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1, category:"Thriller"},
 	{ id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2, category:"Crime"},
 	{ id:3, title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3, category:"Crime"},
